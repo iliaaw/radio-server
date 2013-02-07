25533dfa081bca3b46fff4f7a93607201141b8ef
Ilya <ilyahohryakov@gmail.com>
2013-02-07 23:36:50 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kigendan::Application.config.secret_token = '2cce2828d28f96b8bac7ab18abcfc19454601e3b898551ded9a61847ab5fa613d914d504ba4264059496efca8492d9b6a2d3633e69a9248efb722473d00b4829'
