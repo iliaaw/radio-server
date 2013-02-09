11a3373a358bdce0afc7d0f254ad53177bea2f34
Ilya <ilyahohryakov@gmail.com>
2013-02-10 00:38:16 +0400

    Added server-side file type validation and metadata extraction

--- app/uploaders/track_uploader.rb
+++ app/uploaders/track_uploader.rb
@@ -1,21 +1,9 @@
-# encoding: utf-8
+require 'taglib'
 
 class TrackUploader < CarrierWave::Uploader::Base
 
-  # Include RMagick or MiniMagick support:
-  # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
-
-  # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
-  # include Sprockets::Helpers::RailsHelper
-  # include Sprockets::Helpers::IsolatedHelper
-
-  # Choose what kind of storage to use for this uploader:
   storage :file
-  # storage :fog
 
-  # Override the directory where uploaded files will be stored.
-  # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
@@ -28,23 +16,21 @@ class TrackUploader < CarrierWave::Uploader::Base
   #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
   # end
 
-  # Process files as they are uploaded:
-  # process :scale => [200, 300]
-  #
-  # def scale(width, height)
-  #   # do something
-  # end
+  process :extract_metadata
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  def extract_metadata
+    TagLib::FileRef.open(file.path) do |ref|
+      tag = ref.tag
+      model.album = tag.album
+      model.author = tag.artist
+      model.genre = tag.genre
+      model.title = tag.title
+    end
+  end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(mp3 ogg)
+  end
 
   def filename
     "#{upload_token}.#{file.extension}"
