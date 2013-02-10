0f7a9b1c2101497bfa9b7e0ccb7a3819950d058e
Ilya <ilyahohryakov@gmail.com>
2013-02-10 14:41:36 +0400

    Fixed a bug in a model field naming

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,4 @@
 class Track < ActiveRecord::Base
-  attr_accessible :album, :author, :bitrate, :duration, :genre, :release_date, :title, :upload, :upload_token
+  attr_accessible :album, :artist, :bitrate, :duration, :genre, :release_date, :title, :upload, :upload_token
   mount_uploader :upload, TrackUploader
 end
