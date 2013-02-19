239e9d31442c268af98f0c4a44b6bf77fe06c358
Ilya <ilyahohryakov@gmail.com>
2013-02-20 01:36:13 +0400

    Design fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def nav_link(link_text, link_path)
+    class_name = 'nav-item ' + (current_page?(link_path) ? 'nav-item-active' : '')
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
+
 end
