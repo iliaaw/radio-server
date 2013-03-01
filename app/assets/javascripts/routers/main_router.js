6c0bc5ea11ed697c75cee5f8c6dbdb35b2df135f
Ilya <ilyahohryakov@gmail.com>
2013-03-01 19:39:13 +0600

    Implemented playlist's tracks bootstrapping

--- app/assets/javascripts/routers/main_router.js
+++ app/assets/javascripts/routers/main_router.js
@@ -22,6 +22,7 @@ Kigendan.Routers.Main = Backbone.Router.extend({
 
     newTrack: function() {
         var uploader = new Kigendan.Views.TrackUploader();
+        uploader.render();
     },
 
     playlistsIndex: function(page) {
@@ -34,12 +35,15 @@ Kigendan.Routers.Main = Backbone.Router.extend({
         window._gPlaylist = new Kigendan.Models.PlaylistWithTracks();
         var playlistView = new Kigendan.Views.PLaylistWithTracks({ model: window._gPlaylist });
         var tracksSearch = new Kigendan.Views.TracksSearch();
+        playlistView.render();
+        tracksSearch.render();
     },
 
     showPlaylist: function(playlistId) {
-        var playlist = new Kigendan.Models.PlaylistWithTracks({ id: playlistId });
+        var playlist = new Kigendan.Models.PlaylistWithTracks(window._gPlaylist);
+        playlist.tracks.reset(window._gPlaylist.tracks)
         var playlistView = new Kigendan.Views.PLaylistWithTracks({ model: playlist });
-        playlist.fetch();
+        playlistView.render();
     }
 
 });
