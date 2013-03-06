a7e13b9738c1cdc0208a7216ceee81e6a41d3ce9
Ilya <ilyahohryakov@gmail.com>
2013-03-06 22:03:54 +0600

    Changed uploads dir

--- app/uploaders/track_uploader.rb
+++ app/uploaders/track_uploader.rb
@@ -8,14 +8,6 @@ class TrackUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  # Provide a default URL as a default if there hasn't been a file uploaded:
-  # def default_url
-  #   # For Rails 3.1+ asset pipeline compatibility:
-  #   # asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
-  #
-  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
-  # end
-
   process :extract_metadata
 
   def extract_metadata
@@ -37,6 +29,7 @@ class TrackUploader < CarrierWave::Uploader::Base
   end
 
   protected
+  
   def upload_token
     model.upload_token ||= Digest::MD5.hexdigest(Time.now.to_i.to_s)
   end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,1 +1,5 @@
 CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.]/
+
+CarrierWave.configure do |config|
+  config.root = Rails.root
+end
